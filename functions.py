6be687b7376e7c93657a5ae57b9001bc99d28ce0
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-28 15:56:23 +0100

    :hammer: Update scripts

--- functions.py
+++ functions.py
@@ -1,19 +1,36 @@
 # Import librairies
-from exif import Image
-
-# Files contain all functions use for the script main.py 
-
+import exif
 
 #check if the file is an img
 def is_img(file):
-  return img.has_exif 
+  return file.has_exif 
 
 
 # Collect exif we want to have and place it in dict
-def exif_collect(): 
+def exif_collect(img, file): 
+
+  name = file
+
+  file_format = file.split('.') # On sépare le nom du format dans une liste
+  file_format = file_format[-1] # On ne garde que le dernier élément de la liste, qui correspond au format
+
+  date = img.get('datetime').split()
+  date = date[0].split(':')
+  date = "-".join(date)
+  
+  location = [img.get('gps_longitude'), img.get('gps_latitude')]
+  
+  for i in range (len(location)):
+    if location[i] == None:
+      location = 'location-inconnu'
+
+  if location == list:
+    location = "-".join(location)
+
   img_data = {
     'name': file,
-    'date': img.get('datetime_original'),
-    'location': [img.get('gps_longitude'), img.get('gps_latitude')]
+    'format': file_format,
+    'date': date,
+    'location': location
   }
   return img_data
--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 # Import librairies
 import os, shutil
-
-folder = input('Copy paste link of your pictures folder')
-
-os.mkdir(f'{folder}/others') # On crée un dossier qui va contenir tout ce qui n'est pas une image
+from exif import Image
+from functions import is_img, exif_collect
 
 def pictures_sort(folder):
   files = os.listdir(folder) # On liste tout les éléments se trouvant dans le dossier
 
   for i in range (len(files)): # Parcout de l'entiereté des fichiers
     file = files[i]
-    file_path = f'{folder}/{file}'
+    file_path = f'{folder}\{file}'
 
     with open(file_path, 'rb') as img_file:
       img = Image(img_file)
 
-    if is_img(img) == true:
-      img_data = exif_collect(img)
+    if is_img(img) == True:
+      img_data = exif_collect(img, file)
 
-      img_format = img_data.split('.') # On sépare le nom du format dans une liste
-      img_format = img_format[-1] # On ne garde que le dernier élément de la liste, qui correspond au format
+      os.rename(f'{img_data.get("name")}', f'{img_data.get("location")}_{img_data.get("date")}_{i}.{img_data.get("format")}')
+    
+    # else: #envoyer le fichier dans un dossier avec tout ce qui n'est pas une photo puis retrier les éléments déplacer (dossier à trouver et à renvoyer être trier -> récursivité)
+    #   shutil.move(file_path, f'{folder}\others')
+    #   return pictures_sort(f'{folder}\others')
 
-      os.rename(f'{img_data.get("name")}', f'{img_data.get("location")}_{img_data.get("date")}_{i}.{img_format}')
+    img_file.closed
 
-    else: #envoyer le fichier dans un dossier avec tout ce qui n'est pas une photo puis retrier les éléments déplacer (dossier à trouver et à renvoyer être trier -> récursivité)
-      shutil.move(file_path, f'{folder}/others/{file}')
-      return pictures_sort(f'{folder}/others')
+  return 'All files are sort !'
 
-    return 'All files are sort !'
+
+
+##############################################################################
+# Instruction
+##############################################################################
+
+folder = input('Copy paste link of your pictures folder : ')
+
+pictures_sort(folder)
+
+# os.mkdir(f'{folder}/others') # On crée un dossier qui va contenir tout ce qui n'est pas une image
